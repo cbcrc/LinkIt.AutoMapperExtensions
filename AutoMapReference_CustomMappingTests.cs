abcc78fe24f0a3e6b2e8dae1d44f56cb06e43f53
Laurent le Beau-Martin <laurent.lebeau-martin@radio-canada.ca>
2015-06-17 15:00:33 -0400

    Tests for custom mapping

---
+++ AutoMapReference_CustomMappingTests.cs
@@ -0,0 +1,77 @@
+using ApprovalTests.Reporters;
+using AutoMapper;
+using NUnit.Framework;
+
+namespace ShowMeAnExampleOfAutomapperFromLinkedSource
+{
+    [UseReporter(typeof(DiffReporter))]
+    [TestFixture]
+    public class AutoMapReference_CustomMappingTests
+    {
+        [Test]
+        public void MapModel_WithCustomMappingForOtherProperty_ShouldMap()
+        {
+            Mapper
+                .CreateMap<MyCustomLinkedSource, MyCustomDto>()
+                .MapModel()
+                .ForMember(destination => destination.SelfUrl, opt => opt.MapFrom(src => "http://blah.com/" + src.Model.Id));
+
+            Mapper.AssertConfigurationIsValid();
+
+            var source = new MyCustomLinkedSource
+            {
+                Model = CreateMyCustomModel(1),
+            };
+                
+            var actual = Mapper.Map<MyCustomDto>(source);
+
+            Assert.That(actual.SelfUrl, Is.EqualTo("http://blah.com/" + source.Model.Id));
+        }
+
+        [Test]
+        public void MapModel_WithCustomMappingForModelProperty_ShouldMap()
+        {
+            Mapper
+                .CreateMap<MyCustomLinkedSource, MyCustomDto>()
+                .MapModel()
+                .ForMember(destination => destination.Title, opt => opt.MapFrom(src => src.Model.Title + " TEST"))
+                .ForMember(destination => destination.SelfUrl, opt => opt.Ignore());
+
+            Mapper.AssertConfigurationIsValid();
+
+            var source = new MyCustomLinkedSource
+            {
+                Model = CreateMyCustomModel(1),
+            };
+
+            var actual = Mapper.Map<MyCustomDto>(source);
+
+            Assert.That(actual.Title, Is.EqualTo(source.Model.Title + " TEST"));
+        }
+
+        private static MyCustomModel CreateMyCustomModel(int id)
+        {
+            return new MyCustomModel{
+                Id = id,
+                Title = "Title "+id,
+            };
+        }
+
+        public class MyCustomLinkedSource
+        {
+            public MyCustomModel Model { get; set; }
+        }
+
+        public class MyCustomModel {
+            public int Id { get; set; }
+            public string Title { get; set; }
+        }
+
+        public class MyCustomDto {
+            public string SelfUrl { get; set; }
+            public string Title { get; set; }
+        }
+
+
+    }
+}
