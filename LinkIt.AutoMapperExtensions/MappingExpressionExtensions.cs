0deeb3973c84436c4a8f442ee257df3361d74cca
Stephane Leblanc <stephane.leblanc@radio-canada.ca>
2016-04-19 17:11:00 -0400

    Add license headers

--- LinkIt.AutoMapperExtensions/MappingExpressionExtensions.cs
+++ LinkIt.AutoMapperExtensions/MappingExpressionExtensions.cs
@@ -1,4 +1,9 @@
-﻿using AutoMapper;
+﻿#region copyright
+// Copyright (c) CBC/Radio-Canada. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+#endregion
+
+using AutoMapper;
 
 namespace LinkIt.AutoMapperExtensions
 {
